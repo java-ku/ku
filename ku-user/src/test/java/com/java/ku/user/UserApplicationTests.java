67864f891852fd873cc883e2eb97da70168e1f04
uladzislau.kaliaha <uladzislau.kaliaha@noogadev.com>
2022-08-08 02:31:36 +0400

    created init projects

---
+++ ku-user/src/test/java/com/java/ku/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.ku.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
