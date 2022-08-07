070cc06772ef6ca0506db745aa78d9de6c064cbe
uladzislau.kaliaha <uladzislau.kaliaha@noogadev.com>
2022-08-07 22:46:14 +0400

    check health test

--- ku-employee/src/test/java/com/java/ku/employee/EmployeeApplicationTests.java
+++ ku-employee/src/test/java/com/java/ku/employee/EmployeeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.java.ku.employee;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,7 @@ class EmployeeApplicationTests {
 
     @Test
     void contextLoadsTest() {
-        Assertions.assertTrue(false);
+
     }
 
 }
