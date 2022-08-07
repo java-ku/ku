19bfa9b4087a064faf2d7d0844f70efa744aa9a5
uladzislau.kaliaha <uladzislau.kaliaha@noogadev.com>
2022-08-07 20:21:32 +0400

    init

---
+++ ku-employee/src/test/java/com/java/ku/employee/EmployeeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.java.ku.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+    @Test
+    void contextLoadsTest() {
+
+    }
+
+}
